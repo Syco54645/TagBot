268985d54008025d81adc7a9ca69f231c01a1f7c
Syco54645 <frank@splra.org>
2021-10-22 21:47:35 -0400

    clean up unneeded files in the api

--- TagBot.Api/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace TagBot.Api1.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
